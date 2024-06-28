fdc953d0bebd81889ffbb2de76dff1c8b95a512e
outdrumz <barabanshikoff@gmail.com>
2024-06-28 17:47:06 +0400

    updated design with header

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,54 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function Home() {
-    return (<div>
-    <Image
-          src="/static/LogoRG.png"
-          width={300}
-          height={120}
-          alt="logo"
-          priority={true}
-        />
+    return (
+      <div>
+        <div className="min-h-screen flex flex-col">
+        <header className="top-0 left-0 z-20 p-4 w-full flex items-center">
+
+            <h1 className="flex flex-col">
+              <span className="block">Roller</span>
+              <span className="block -mt-2 sm:-mt-2 md:-mt-1">Graphics</span>
+              <div className="flex justify-between mt-1 sm:mt-1 md:mt-2 max-w-full">
+                <div className="w-8 h-4 -mt-0.5 sm:w-8 sm:h-4 md:w-12 md:h-7 bg-orange-951"></div>
+                <div className="w-8 h-4 -mt-0.5 sm:w-8 sm:h-4 md:w-12 md:h-7 bg-red-951"></div>
+                <div className="w-8 h-4 -mt-0.5 sm:w-8 sm:h-4 md:w-12 md:h-7 bg-blue-951"></div>
+              </div>
+            </h1>
+            <div>
+            <div className="w-12 sm:w-12 md:w-16">
+              <Image
+                  src="/static/indianroller.png"
+                  layout="responsive"
+                  width={212}
+                  height={352}
+                  alt="logo image"
+                  priority={true}
+                  className="object-contain"
+                  />
+            </div>
+            </div>
+
+            <nav className="ml-auto space-x-1 md:space-x-5 -mt-6">
+              <a href="#lineup" className="hidden md:inline hover:underline">LINEUP</a>
+              <a href="#documentation" className="hidden md:inline hover:underline">DOCUMENTATION</a>
+              <a href="#demo" className="hidden md:inline hover:underline">DEMO</a>
+              <a href="#login" className="hidden md:inline hover:underline">LOGIN</a>
+              <a href="#register" className="hidden md:inline hover:underline">REGISTER</a>
+              </nav>
+          
+        </header>
+        <main>
+            <video className="w-full" autoPlay>
+                            <source src="/static/video.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+            </video>
+        </main>
+        <footer>
+          <p className="text-white text-center">© 2024 My Website</p>
+        </footer>
         </div>
+
+      </div>
     );
 } 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        gradient: "url('/static/background.webp')",
+      colors: {
+        purple: {
+          951: 'rgba(79, 70, 229, 1)',
+        },
+        gray: {
+          950: 'hsla(0, 0%, 100%, 0.7);',
+          951: 'hsla(0, 0%, 100%, 0.4);',
+          952: '#373b64',
+        },
+        orange: {
+          951: '#EB8A18',
+        },
+        red: {
+          951: '#EB183E',
+        },
+        blue: {
+          951: '#186DEB',
+        },                
+        linen: {
+          951: '#F8EFE6',
+        },  
       },
     },
   },
